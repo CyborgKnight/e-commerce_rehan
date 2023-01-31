993650b5036136c63877ddd5be6839f25eb3446f
Raihan Maulana <raihanmantap15@gmail.com>
2023-01-31 10:25:12 +0700

    login page

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="asset/css/bootstrap.min.css">
+    <title>toko onlen</title>
+</head>
+
+<body>
+    <br><br><br>
+    <div class="container">
+        <div class="card">
+            <div class="card-body">
+                <form>
+                    <div class="mb-3">
+                        <label class="form-label"> Email </label>
+                        <input type="email" class="form-control" id="" aria-describedby="emailHelp">
+                    </div>
+                    <div class="mb-3">
+                        <label class="form-label"> Password </label>
+                        <input type="password" class="form-control" id="">
+                    </div>
+                    <a href="dashboard.php" type="submit" class="btn btn-primary"> Login </a>
+                </form>
+            </div>
+            <div class="card-body">
+                <label class="">Belum punya akun?</label>
+                <a href="register.php" type="submit" class="btn btn-danger" target="_blank"> Register </a>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+<script src="asset/js/bootstrap.bundle.js">
+
+</script>
+
+</html>
---
+++ register.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="asset/css/bootstrap.min.css">
+    <title>toko onlen</title>
+</head>
+
+<body>
+    <br><br><br>
+    <div class="card">
+        <div class="card-body">
+            
+        </div>
+    </div>
+</body>
+
+</html>
