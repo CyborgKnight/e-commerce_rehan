b195ab0335587772a7b96bed89970b38f32d9e9f
Raihan Maulana <raihanmantap15@gmail.com>
2023-01-31 19:52:57 +0700

    in

--- register.php
+++ register.php
@@ -25,7 +25,7 @@
                                     <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z" />
                                 </svg>
                             </div>
-                            <input type="text" name="nama" class="form-control" placeholder="Nama Lengkap" required>
+                            <input type="text" name="nama" class="form-control" placeholder="Nama Lengkap" autocomplete="off" required>
                         </div>
                         <br>
                         <div class="input-group">
@@ -35,7 +35,7 @@
                                     <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
                                 </svg>
                             </div>
-                            <input type="text" name="username" class="form-control" placeholder="Username" required>
+                            <input type="text" name="username" class="form-control" placeholder="Username" autocomplete="off" required>
                         </div>
                         <br>
                         <div class="input-group">
@@ -44,7 +44,7 @@
                                     <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z" />
                                 </svg>
                             </div>
-                            <input type="text" name="email" class="form-control" placeholder="Email" required>
+                            <input type="text" name="email" class="form-control" placeholder="Email" autocomplete="off"b required>
                         </div>
                         <br>
                         <div class="input-group">
